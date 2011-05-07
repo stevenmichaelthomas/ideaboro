baa4adb392db6829bd042acc534fff58fdf6ee8c
frika <steve.michael.thomas@gmail.com>
2011-05-07 12:06:10 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ideaborough::Application.config.secret_token = '78816bed44c08c9c6b15e11483e0921808f4c006c2f90dea40641a4a231793f53e032fe0e0c38f3f3cdfee01f7bce8fab4c5c81227ddcc86b7b86f0be3776782'
