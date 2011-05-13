6fb37a3ac395faf50f1f846765d1ffc3e3a8d1d0
frika <steve.michael.thomas@gmail.com>
2011-05-13 09:36:11 +0800

    Chapter 4 done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+#Page specific titles -ST
+def title
+	base_title = "Ideaborough"
+		return base_title if @title.nil?
+		return "#{base_title} | #{@title}"
+end
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -2,6 +2,10 @@ require 'spec_helper'
 
 describe PagesController do
 	render_views
+	
+	before(:each) do
+		@base_title = "Ideaborough"
+	end
 
   describe "GET 'home'" do
     it "should be successful" do
@@ -11,7 +15,7 @@ describe PagesController do
 		it "should have the right title" do
 			get 'home'
 			response.should have_selector("title",
-                    :content => "Ideaborough | Home")
+                    :content => @base_title + " | Home")
 		end
     end
 
@@ -24,7 +28,7 @@ describe PagesController do
 		it "should have the right title" do
 			get 'contact'
 			response.should have_selector("title",
-                    :content => "Ideaborough | Contact")
+                    :content => @base_title + " | Contact")
 		end
 	end
 	describe "GET 'about'" do
@@ -35,7 +39,18 @@ describe PagesController do
 		it "should have the right title" do
 			get 'about'
 			response.should have_selector("title",
-                    :content => "Ideaborough | About")
+                    :content => @base_title + " | About")
+		end
+	end  
+	describe "GET 'help'" do
+    it "should be successful" do
+      get 'help'
+      response.should be_success
+    end
+		it "should have the right title" do
+			get 'help'
+			response.should have_selector("title",
+                    :content => @base_title + " | Help")
 		end
 	end  
 end
