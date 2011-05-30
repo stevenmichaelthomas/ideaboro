51ca960a5f46770040d9071f79a627b979f80c89
frika <steve.michael.thomas@gmail.com>
2011-05-30 22:19:38 +0800

    test

--- db/migrate/20110525064051_remove_comment_from_micropost.rb
+++
@@ -1,9 +0,0 @@
-class RemoveCommentFromMicropost < ActiveRecord::Migration
-  def self.up
-    remove_column :microposts, :comment
-  end
-
-  def self.down
-    add_column :microposts, :comment, :string
-  end
-end
